2f55a40b2c8ca7469bd41574ddee1cbd7f5efcca
DoctorJuj <DoctorJuj555@GMAIL.COM>
2022-08-03 21:17:06 +0200

    add log for Activity lifecycle works and add Save and restore the Activity instance state

--- app/src/main/java/com/example/twoactivities/MainActivity.kt
+++ app/src/main/java/com/example/twoactivities/MainActivity.kt
@@ -2,12 +2,14 @@ package com.example.twoactivities
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 const val TEXT_REQUEST = 1
+
 class MainActivity : AppCompatActivity() {
     companion object {
         const val EXTRA_MESSAGE: String = "com.example.android.twoactivities.extra.MESSAGE"
@@ -21,23 +23,71 @@ class MainActivity : AppCompatActivity() {
         mMessageEditText = findViewById(R.id.editText_main)
         mReplyHeadTextView = findViewById(R.id.text_header_reply)
         mReplyTextView = findViewById(R.id.text_message_reply)
+        if (savedInstanceState != null) {
+            val isVisible = savedInstanceState.getBoolean("reply_visible")
+            if (isVisible) {
+                mReplyHeadTextView?.visibility = View.VISIBLE
+                mReplyTextView?.text = savedInstanceState.getString("reply_text")
+                mReplyTextView?.visibility = View.VISIBLE
+            }
+        }
+        Log.d("MainActivity", "-------")
+        Log.d("MainActivity", "onCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("MainActivity", "onStart")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("MainActivity", "onPause")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("MainActivity", "onResume")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d("MainActivity", "onRestart")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("MainActivity", "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("MainActivity", "onDestroy")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        if (mReplyHeadTextView?.visibility == View.VISIBLE) {
+            outState.putBoolean("reply_visible", true)
+            outState.putString("reply_text", mReplyTextView?.text.toString())
+        }
     }
 
     fun launchSecondActivity(view: View) {
         val intent = Intent(this, SecondActivity::class.java)
         val message = mMessageEditText!!.text.toString()
         intent.putExtra(EXTRA_MESSAGE, message)
-        startActivityForResult(intent, TEXT_REQUEST);
+        startActivityForResult(intent, TEXT_REQUEST)
     }
 
-    override fun onActivityResult(requestCode: Int,resultCode: Int, data: Intent?) {
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == TEXT_REQUEST) {
             if (resultCode == RESULT_OK) {
                 val reply: String? = data?.getStringExtra(SecondActivity.EXTRA_REPLY)
-                mReplyHeadTextView?.setVisibility(View.VISIBLE)
-                mReplyTextView?.setText(reply)
-                mReplyTextView?.setVisibility(View.VISIBLE)
+                mReplyHeadTextView?.visibility = View.VISIBLE
+                mReplyTextView?.text = reply
+                mReplyTextView?.visibility = View.VISIBLE
             }
         }
     }
--- app/src/main/java/com/example/twoactivities/SecondActivity.kt
+++ app/src/main/java/com/example/twoactivities/SecondActivity.kt
@@ -2,11 +2,11 @@ package com.example.twoactivities
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-
 
 class SecondActivity : AppCompatActivity() {
     companion object {
@@ -19,15 +19,48 @@ class SecondActivity : AppCompatActivity() {
         mReply = findViewById(R.id.editText_second)
         val intent = intent
         val message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE)
-        val textView :TextView = findViewById(R.id.text_message)
-        textView.setText(message)
+        val textView: TextView = findViewById(R.id.text_message)
+        textView.text = message
+        Log.d("SecondActivity", "-------")
+        Log.d("SecondActivity", "onCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("SecondActivity", "onStart")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("SecondActivity", "onPause")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("SecondActivity", "onResume")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d("SecondActivity", "onRestart")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("SecondActivity", "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("SecondActivity", "onDestroy")
     }
 
     fun returnReply(view: View) {
         val reply = mReply!!.text.toString()
         val replyIntent = Intent()
         replyIntent.putExtra(EXTRA_REPLY, reply)
-        setResult(RESULT_OK,replyIntent)
+        setResult(RESULT_OK, replyIntent)
+        Log.d("SecondActivity", "End SecondActivity")
         finish()
     }
 }
